fc4cf8a2a64206befbda0d06f0cd39369edcbafe
RobinMC <robinslot@hotmail.nl>
2017-08-06 23:01:03 +0200

    Platform rewrite

--- src/xyz/derkades/minigames/games/Platform.java
+++ src/xyz/derkades/minigames/games/Platform.java
@@ -1,6 +1,8 @@
 package xyz.derkades.minigames.games;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
@@ -12,7 +14,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
 
@@ -20,13 +21,15 @@ import net.md_5.bungee.api.ChatColor;
 import xyz.derkades.derkutils.bukkit.ItemBuilder;
 import xyz.derkades.minigames.Minigames;
 import xyz.derkades.minigames.Var;
-import xyz.derkades.minigames.utils.Console;
-import xyz.derkades.minigames.utils.Scheduler;
 import xyz.derkades.minigames.utils.Utils;
 
 public class Platform extends Game {
-
-	private HashMap<String, Boolean> isDead = new HashMap<>();
+	
+	private static final int SPREAD_TIME = 5;
+	private static final int GAME_DURATION = 40;
+	private static final int KNOCKBACK_SWORDS_TIME = 20;
+	
+	private static final String SECONDS_LEFT = "%s seconds left.";
 	
 	@Override
 	public String[] getDescription(){
@@ -34,7 +37,7 @@ public class Platform extends Game {
 				"Platform is pretty self explanatory, you spawn",
 				"on a platform. The goal of this game is to try",
 				"to knock off other players from the platform.",
-				"At the start of the game and at the 10 seconds",
+				"At the start of the game and at the " + KNOCKBACK_SWORDS_TIME + " seconds",
 				"left mark you will get a knockback sword, which",
 				"you can only use once."
 		};
@@ -56,32 +59,56 @@ public class Platform extends Game {
 	}
 	
 	@Override
-	public void resetHashMaps(Player player){
-		isDead.put(player.getName(), false);
-	}
+	public void resetHashMaps(Player player){}
+	
+	private List<UUID> dead;
 	
 	@Override
 	public void begin(){
-		//Teleport everyone to the arena and set them as not dead
+		dead = new ArrayList<>();
+		
 		for (Player player : Bukkit.getOnlinePlayers()){
-			player.teleport(new Location(Var.WORLD, 229.5, 89, 199.5, 180, 90));
-			isDead.put(player.getName(), false);
-			
-			player.addPotionEffect(new PotionEffect(
-					PotionEffectType.DAMAGE_RESISTANCE, 100000, 255, true));
-			player.addPotionEffect(new PotionEffect(
-					PotionEffectType.NIGHT_VISION, 100000, 255, true));
+			player.teleport(new Location(Var.WORLD, 229.5, 88, 186.5, 0, 0));
+			Utils.giveInfiniteEffect(player, PotionEffectType.DAMAGE_RESISTANCE, 255);
 		}
 		
-		startGameTimer();
+		sendMessage("You have " + SPREAD_TIME + " seconds to spread before the game starts.");
 		
-		new BukkitRunnable(){
-			public void run(){
-				for (Player player : Bukkit.getOnlinePlayers()){
-					Minigames.setCanTakeDamage(player, true); //Allow PvP
+		new BukkitRunnable() {
+			
+			int secondsLeft = GAME_DURATION + SPREAD_TIME;
+			
+			@Override
+			public void run() {
+				if (secondsLeft == GAME_DURATION) {
+					for (Player player : Bukkit.getOnlinePlayers()) {
+						Minigames.setCanTakeDamage(player, true);
+					}
+					sendMessage("The game has started.");
 				}
+				
+				if (Utils.getAliveCountFromDeadList(dead) <= 1 && secondsLeft > 2) {
+					secondsLeft = 2;
+				}
+				
+				if (secondsLeft == 20) {
+					giveSwords();
+				}
+				
+				if (secondsLeft <= 0) {
+					this.cancel();
+					endGame();
+					return;
+				}
+				
+				if (secondsLeft == 15 || secondsLeft <= 5) {
+					sendMessage(String.format(SECONDS_LEFT, secondsLeft));
+				}
+				
+				secondsLeft--;
 			}
-		}.runTaskLater(Minigames.getInstance(), 3*20);
+			
+		}.runTaskTimer(Minigames.getInstance(), 0, 20);
 	}
 	
 	private void giveSwords(){
@@ -91,52 +118,28 @@ public class Platform extends Game {
 				.lore(ChatColor.AQUA + "You can only use this once!")
 				.enchant(Enchantment.KNOCKBACK, 1)
 				.create();
+		
 		for (Player player: Bukkit.getOnlinePlayers()){
-			if (!isDead.get(player.getName())){ //Don't give sword to spectators
+			if (!dead.contains(player.getUniqueId())){ //Don't give sword to spectators
 				player.getInventory().setItem(0, sword);
 			}
 		}
 	}
 	
-	private void startGameTimer(){
-		Scheduler.runTaskLater(5*20, new Runnable(){
-			public void run(){
-				sendMessage("20 seconds left!");
-				Scheduler.runTaskLater(10*20, new Runnable(){
-					public void run(){
-						sendMessage("10 seconds left!");
-						giveSwords(); //Give everyone who is still alive a sword
-						Scheduler.runTaskLater(5*20, new Runnable(){
-							public void run(){
-								sendMessage("5 seconds left!");
-								Scheduler.runTaskLater(5*20, new Runnable(){
-									public void run(){
-										endGame();
-									}
-								});
-							}
-						});
-					}
-				});
-			}
-		});
-	}
-	
 	private void endGame(){
-		Console.sendCommand("setblock 228 77 217 redstone_block"); //Activates fireworks command blocks
-	
-		sendMessage("Game has ended!");
-		
-		super.startNextGame(Utils.getWinnersFromIsDeadHashMap(isDead));
+		//Console.sendCommand("setblock 228 77 217 redstone_block"); //Activates fireworks command blocks
+		sendMessage("The game has ended!");
+		super.startNextGame(Utils.getWinnersFromDeadList(dead));
 	}
 	
 	private void playerDie(Player player){
 		sendMessage(player.getName() + " has been eliminated from the game!");
-		Location loc = player.getLocation();
-		Var.WORLD.spigot().strikeLightning(new Location(Var.WORLD, loc.getX(), loc.getY(), loc.getZ()), false);
-		player.teleport(new Location(Var.WORLD, 224, 95, 199, -90, 0));
-		isDead.put(player.getName(), true);
+		Var.WORLD.spigot().strikeLightningEffect(player.getLocation(), false);
+		player.teleport(new Location(Var.WORLD, 229.5, 93, 178.0, 0, 0));
+		dead.add(player.getUniqueId());
 		player.getInventory().clear();
+		Utils.giveInvisibility(player);
+		player.setAllowFlight(true);
 		
 		Minigames.setCanTakeDamage(player, false); //Disallow PvP
 	}
@@ -144,6 +147,11 @@ public class Platform extends Game {
 	@EventHandler(priority = EventPriority.MONITOR)
 	public void onMove(PlayerMoveEvent event){
 		Player player = event.getPlayer();
+		
+		if (dead.contains(player.getUniqueId())) {
+			return;
+		}
+		
 		if(event.getTo().getBlock().getRelative(BlockFace.DOWN).getType() == Material.STAINED_CLAY){
 			playerDie(player);
 		}
