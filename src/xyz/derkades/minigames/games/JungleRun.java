623c6b8d91f3d628883e1957157806f89a4b7c6e
Robin <robinslot@hotmail.nl>
2017-08-15 13:13:33 +0200

    JungleRun optimisations

--- src/xyz/derkades/minigames/games/JungleRun.java
+++ src/xyz/derkades/minigames/games/JungleRun.java
@@ -1,8 +1,7 @@
 package xyz.derkades.minigames.games;
 
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
+import java.util.UUID;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
@@ -14,8 +13,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import xyz.derkades.minigames.Minigames;
@@ -33,16 +30,13 @@ public class JungleRun extends ParkourGame implements Listener {
 						"of the game."
 				}, 1, 2, 4);
 	}
-
-	private HashMap<String, Boolean> hasFinished = new HashMap<>();
-	private HashMap<String, Boolean> isSpectator = new HashMap<>();
+	
+	private List<UUID> finished;
+	private List<UUID> spectator;
 
 	@Override
 	void begin() {
-		for (Player player: Bukkit.getOnlinePlayers()){
-			hasFinished.put(player.getName(), false);
-			isSpectator.put(player.getName(), false);
-			
+		for (Player player: Bukkit.getOnlinePlayers()){		
 			player.teleport(new Location(Var.WORLD, 282.5, 67, 196.5, -90, 0)); //Teleport all online players to the arena
 		}
 		timer();
@@ -54,51 +48,54 @@ public class JungleRun extends ParkourGame implements Listener {
 				sendMessage("5 seconds left!");
 				new BukkitRunnable(){
 					public void run(){
-						endGame(); //End the game after 25 seconds
+						startNextGame(Utils.getPlayerListFromUUIDList(finished));
 					}
 				}.runTaskLater(Minigames.getInstance(), 5 * 20);
 			}
 		}.runTaskLater(Minigames.getInstance(), 20 * 20);
 	}
 	
-	private void endGame(){
-		super.startNextGame(Utils.getWinnersFromFinishedHashMap(hasFinished));
-	}
-	
 	private void playerDie(Player player){
 		super.sendMessage(player.getName() + " has been eliminated from the game!");
-		player.teleport(new Location(Var.WORLD, 296.5, 80, 204.5, 180, 0)); //Teleport player to spectator room
-		player.setAllowFlight(true);
-		isSpectator.put(player.getName(), true);
-		//Console.sendCommand("effect " + player.getName() + " invisibility 10000 1 true");
-		player.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 100000, 0, true));
+		player.teleport(new Location(Var.WORLD, 296.5, 80, 204.5, 180, 0));
+		
+		spectator(player);
 	}
 	
 	private void playerWin(Player player){
-		super.sendMessage(player.getName() + " has made it to the finish!"); //Send message
-		player.teleport(new Location(Var.WORLD, 296.5, 80, 204.5, 180, 0)); //Teleports player to the spectator room
-		super.sendConsoleCommand("particle flame 327.5 72 196.5 0 2 2 0.1 1000"); //Plays particle effect at finish
+		super.sendMessage(player.getName() + " has made it to the finish!");
+		
+		player.teleport(new Location(Var.WORLD, 296.5, 80, 204.5, 180, 0));
+		
+		//Play particle effect and sound
+		super.sendConsoleCommand("particle flame 327.5 72 196.5 0 2 2 0.1 1000");
 		player.playSound(player.getLocation(), Sound.LEVEL_UP, 1.0f, 1.0f);
-		hasFinished.put(player.getName(), true);
-		isSpectator.put(player.getName(), true);
-		player.setAllowFlight(true);
+		
+		finished.add(player.getUniqueId());
+		
+		spectator(player);
 	}
 	
-	@EventHandler(priority = EventPriority.MONITOR)
+	private void spectator(Player player) {
+		player.setAllowFlight(true);
+		spectator.add(player.getUniqueId());
+		Utils.giveInvisibility(player);
+	}
+	
+	@EventHandler(priority = EventPriority.HIGH)
 	public void onMove(PlayerMoveEvent event){
 		Player player = event.getPlayer();
-		if(!isSpectator.get(player.getName())){ //If the player is not a spectator
-			Material type = event.getTo().getBlock().getRelative(BlockFace.DOWN).getType();
-			List<Material> die = Arrays.asList(
-					Material.WOOL, 
-					Material.SOUL_SAND, 
-					Material.STAINED_CLAY
-					);
-			if (die.contains(type)){
-				playerDie(player);
-			} else if(type == Material.GOLD_BLOCK){
-				playerWin(player); //If on gold block -> win
-			}	
+		
+		if (spectator.contains(player.getUniqueId())){
+			return;	
+		}
+		
+		Material type = event.getTo().getBlock().getRelative(BlockFace.DOWN).getType();
+		
+		if (type == Material.WOOL || type == Material.SOUL_SAND || type == Material.STAINED_CLAY){
+			playerDie(player);
+		} else if(type == Material.GOLD_BLOCK){
+			playerWin(player);
 		}
 	}
 
