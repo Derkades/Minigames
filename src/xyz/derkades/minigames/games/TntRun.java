00f397e6315d5974cc8d529c83f5abd423dee762
Derkades <robinslot@hotmail.nl>
2017-11-04 21:31:36 +0100

    RETURN

--- src/xyz/derkades/minigames/games/TntRun.java
+++ src/xyz/derkades/minigames/games/TntRun.java
@@ -77,6 +77,7 @@ public class TntRun extends Game {
 			player.setAllowFlight(true);
 			Bukkit.getOnlinePlayers().forEach((online) -> online.hidePlayer(player));
 			alive.remove(player.getUniqueId());
+			return;
 		}
 		
 		if (block.getType() != map.floorMaterial()) {
