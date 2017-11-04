7837823a8b255a65b24e1379fe739430e8f0e121
Derkades <robinslot@hotmail.nl>
2017-11-04 20:27:31 +0100

    Future map

---
+++ src/xyz/derkades/minigames/tntrun/Future.java
@@ -0,0 +1,29 @@
+package xyz.derkades.minigames.tntrun;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+
+import xyz.derkades.minigames.Var;
+import xyz.derkades.minigames.utils.BlockUtils;
+
+public class Future extends TNTMap {
+
+	@Override
+	public void restore() {
+		BlockUtils.fillArea(261, 80, 119, 273, 80, 131, Material.TNT);
+		BlockUtils.fillArea(261, 74, 119, 273, 80, 131, Material.TNT);
+		BlockUtils.fillArea(261, 68, 119, 273, 80, 131, Material.TNT);
+		BlockUtils.fillArea(261, 62, 119, 273, 80, 131, Material.TNT);
+	}
+
+	@Override
+	public Material floorMaterial() {
+		return Material.TNT;
+	}
+
+	@Override
+	public Location spawnLocation() {
+		return new Location(Var.WORLD, 137.5 82 124.5);
+	}
+
+}
