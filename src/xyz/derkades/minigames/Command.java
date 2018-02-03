10fbf1410f28a664b28be2dd75b214618f8c72f1
Derkades <robinslot@hotmail.nl>
2018-02-03 22:14:02 +0100

    remove staff related commands

--- src/xyz/derkades/minigames/Command.java
+++ src/xyz/derkades/minigames/Command.java
@@ -1,8 +1,6 @@
 package xyz.derkades.minigames;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.OfflinePlayer;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Fireball;
@@ -13,20 +11,11 @@ import xyz.derkades.minigames.menu.MainMenu;
 
 public class Command implements CommandExecutor {
 
-	@SuppressWarnings("deprecation")
 	@Override
 	public boolean onCommand(CommandSender sender, org.bukkit.command.Command arg1, String arg2, String[] args) {
 		Player player = (Player) sender;
 		
-		if (args.length == 2){
-			if (args[0].equals("staff")){
-				OfflinePlayer target = Bukkit.getOfflinePlayer(args[1]);
-				VIP.setStaff(target, true);
-			} else if (args[0].equals("unstaff")){
-				OfflinePlayer target = Bukkit.getOfflinePlayer(args[1]);
-				VIP.setStaff(target, false);
-			}
-		} else if (args.length == 1){
+		if (args.length == 1){
 			if (args[0].equalsIgnoreCase("start") &&
 					player.isOp()){
 				AutoRotate.startNewRandomGame();
