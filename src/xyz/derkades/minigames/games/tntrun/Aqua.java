85263973342c3d90a40f356e48ffbaa10c0af9e4
Derkades <robinslot@hotmail.nl>
2018-07-21 22:37:04 +0200

    Fill the whole area

--- src/xyz/derkades/minigames/games/tntrun/Aqua.java
+++ src/xyz/derkades/minigames/games/tntrun/Aqua.java
@@ -10,12 +10,11 @@ public class Aqua extends TNTMap {
 
 	@Override
 	public void restore() {
-		BlockUtils.fillArea(261, 83, 98, 273, 83, 110, Material.PACKED_ICE);
-		BlockUtils.fillArea(261, 78, 98, 273, 78, 110, Material.PACKED_ICE);
-		BlockUtils.fillArea(261, 73, 98, 273, 73, 110, Material.PACKED_ICE);
-		BlockUtils.fillArea(261, 68, 98, 273, 68, 110, Material.PACKED_ICE);
-		BlockUtils.fillArea(261, 63, 98, 273, 63, 110, Material.PACKED_ICE);
-		
+		BlockUtils.fillArea(261, 83, 98, 285, 83, 110, Material.PACKED_ICE);
+		BlockUtils.fillArea(261, 78, 98, 285, 78, 110, Material.PACKED_ICE);
+		BlockUtils.fillArea(261, 73, 98, 285, 73, 110, Material.PACKED_ICE);
+		BlockUtils.fillArea(261, 68, 98, 285, 68, 110, Material.PACKED_ICE);
+		BlockUtils.fillArea(261, 63, 98, 285, 63, 110, Material.PACKED_ICE);
 	}
 
 	@Override
