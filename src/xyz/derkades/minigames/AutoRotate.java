ecf227aa2f444b50745c18040a3ddff3196689ee
Derkades <robinslot@hotmail.nl>
2018-11-15 23:23:11 +0100

    Add some explanations and remove "parkour lower chance" code

--- src/xyz/derkades/minigames/AutoRotate.java
+++ src/xyz/derkades/minigames/AutoRotate.java
@@ -7,9 +7,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import net.md_5.bungee.api.ChatColor;
-import xyz.derkades.derkutils.Random;
 import xyz.derkades.minigames.games.Game;
-import xyz.derkades.minigames.games.ParkourGame;
 
 public class AutoRotate {
 
@@ -30,16 +28,11 @@ public class AutoRotate {
 			return;
 		}
 		
-		//If autorotate is enabled and there is at least one player online, continue!
+		//If autorotate is enabled and there are at least two players online, continue!
 		
-		Game game;
-		
-		if (Minigames.NEXT_GAME == null){
-			game = Game.getRandomGame();
-		} else {
-			game = Minigames.NEXT_GAME;
-			Minigames.NEXT_GAME = null;
-		}
+		// If a next game is set by a command, do that game next. Otherwise, pick a random game.
+		Game game = Minigames.NEXT_GAME == null ? Game.getRandomGame() : Minigames.NEXT_GAME;
+		Minigames.NEXT_GAME = null;
 		
 		String lastGame = Minigames.getInstance().getConfig().getString("last-game");
 		//If the randomly selected game is the same as the last game pick a new game
@@ -48,18 +41,10 @@ public class AutoRotate {
 			return;
 		}
 		
-		//If the game is a parkour game, there's a 75% of selecting a new game
-		if (game instanceof ParkourGame){
-			boolean bool = Random.getRandomInteger(0, 3) < 1;
-			if (bool){
-				startNewRandomGame();
-				return;
-			}
-			//Else: continue..
-		}
-		
 		//If there are not enough online players, try again
 		if (!(Bukkit.getOnlinePlayers().size() >= game.getRequiredPlayers())){
+			// This code will never result in a loop. If there are less than 2 players online, this will not run.
+			// There will always be 2 player games.
 			startNewRandomGame();
 			return;
 		}
