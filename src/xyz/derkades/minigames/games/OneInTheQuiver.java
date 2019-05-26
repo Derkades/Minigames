9e451468a5bf921074e26133434c6f5d534ebaf1
Robin <robinslot@hotmail.nl>
2019-05-26 23:46:57 +0200

    Rename to one in the chamber

--- src/xyz/derkades/minigames/games/OneInTheQuiver.java
+++ src/xyz/derkades/minigames/games/OneInTheQuiver.java
@@ -29,22 +29,22 @@ public class OneInTheQuiver extends Game {
 
 	private static final int MAX_GAME_DURATION = 120;
 	private static final int SPREAD_TIME = 5;
-	
+
 	private static final ItemStack SWORD = new ItemBuilder(Material.WOODEN_SWORD)
 			.unbreakable()
 			.create();
-	
+
 	private static final ItemStack BOW = new ItemBuilder(Material.BOW)
 			.unbreakable()
 			.name("Insta-kill bow")
 			.lore("Get arrows by killing other players")
 			.create();
-	
+
 	private static final ItemStack ARROW = new ItemBuilder(Material.ARROW)
 			.create();
-	
+
 	OneInTheQuiver() {
-		super("One in the Quiver", new String[] {
+		super("One in the Chamber", new String[] {
 				"Kill other players with a weak wooden sword.",
 				"For every kill you'll get a single arrow. Arrows",
 				"do enough damage to kill any player instantly."
@@ -52,111 +52,111 @@ public class OneInTheQuiver extends Game {
 	}
 
 	private List<UUID> dead;
-	
+
 	private SniperMap map;
-	
+
 	@Override
-	void begin(GameMap genericMap) {
-		dead = new ArrayList<>();
-		map = (SniperMap) genericMap;
-		
-		for (Player player : Bukkit.getOnlinePlayers()) {
-			player.teleport(map.getSpawnLocation());
+	void begin(final GameMap genericMap) {
+		this.dead = new ArrayList<>();
+		this.map = (SniperMap) genericMap;
+
+		for (final Player player : Bukkit.getOnlinePlayers()) {
+			player.teleport(this.map.getSpawnLocation());
 			player.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 5*20, 0, true, false));
 		}
-		
+
 		new BukkitRunnable() {
-			
+
 			int timeLeft = MAX_GAME_DURATION + SPREAD_TIME;
-			
+
 			@Override
 			public void run() {
-				if (timeLeft > MAX_GAME_DURATION) {
-					sendMessage("The game will start in " + (timeLeft - MAX_GAME_DURATION) + " seconds");
+				if (this.timeLeft > MAX_GAME_DURATION) {
+					OneInTheQuiver.this.sendMessage("The game will start in " + (this.timeLeft - MAX_GAME_DURATION) + " seconds");
 				}
-				
-				if (timeLeft == MAX_GAME_DURATION) {
-					start();
+
+				if (this.timeLeft == MAX_GAME_DURATION) {
+					OneInTheQuiver.this.start();
 				}
-				
-				if (timeLeft == 60 || timeLeft == 30 || timeLeft == 10 || timeLeft < 5) {
-					sendMessage("The game will end in " + timeLeft + " seconds");
+
+				if (this.timeLeft == 60 || this.timeLeft == 30 || this.timeLeft == 10 || this.timeLeft < 5) {
+					OneInTheQuiver.this.sendMessage("The game will end in " + this.timeLeft + " seconds");
 				}
-				
-				if (timeLeft <= 0) {
-					end();
+
+				if (this.timeLeft <= 0) {
+					OneInTheQuiver.this.end();
 					this.cancel();
 				}
-				
-				if (Utils.getAliveCountFromDeadList(dead) < 2) {
-					end();
+
+				if (Utils.getAliveCountFromDeadList(OneInTheQuiver.this.dead) < 2) {
+					OneInTheQuiver.this.end();
 					this.cancel();
 				}
-				
-				timeLeft--;
+
+				this.timeLeft--;
 			}
-			
+
 		}.runTaskTimer(Minigames.getInstance(), 20, 20);
 	}
-	
+
 	private void start() {
-		for (Player player : Bukkit.getOnlinePlayers()) {
+		for (final Player player : Bukkit.getOnlinePlayers()) {
 			Minigames.setCanTakeDamage(player, true);
-			PlayerInventory inv = player.getInventory();
+			final PlayerInventory inv = player.getInventory();
 			inv.addItem(SWORD, BOW, ARROW);
 		}
 	}
-	
+
 	private void end() {
-		List<Player> winners = Utils.getWinnersFromDeadList(dead);
+		final List<Player> winners = Utils.getWinnersFromDeadList(this.dead);
 		if (winners.size() > 1) {
 			winners.clear();
 		}
 		super.endGame(winners);
 	}
-	
+
 	@EventHandler
-	public void onDeath(PlayerDeathEvent event) {
-		Player player = event.getEntity();
-		Player killer = player.getKiller();
+	public void onDeath(final PlayerDeathEvent event) {
+		final Player player = event.getEntity();
+		final Player killer = player.getKiller();
 		killer.getInventory().addItem(ARROW);
-		dead.add(player.getUniqueId());
-		
+		this.dead.add(player.getUniqueId());
+
 		Scheduler.delay(1, () -> {
 			player.spigot().respawn();
-			if (map.getSpectatorLocation() != null) 
-				player.teleport(map.getSpectatorLocation());
+			if (this.map.getSpectatorLocation() != null)
+				player.teleport(this.map.getSpectatorLocation());
 			player.setAllowFlight(true);
 			Utils.giveInvisibility(player);
 			player.getInventory().clear();
 			Minigames.setCanTakeDamage(player, false);
 		});
 	}
-	
+
 	@EventHandler
-	public void onDamage(EntityDamageByEntityEvent event) {
-		Entity damagedEntity = event.getEntity();
-		Entity attackingEntity = event.getDamager();
-		
+	public void onDamage(final EntityDamageByEntityEvent event) {
+		final Entity damagedEntity = event.getEntity();
+		final Entity attackingEntity = event.getDamager();
+
 		if (damagedEntity.getType() != EntityType.PLAYER) {
 			return;
 		}
-		
-		if (dead.contains(attackingEntity.getUniqueId()) || dead.contains(damagedEntity.getUniqueId())) {
+
+		if (this.dead.contains(attackingEntity.getUniqueId()) || this.dead.contains(damagedEntity.getUniqueId())) {
 			return;
 		}
-		
+
 		if (attackingEntity.getType() == EntityType.ARROW) {
 			event.setDamage(20);
 			return;
 		}
-		
+
 		if (attackingEntity.getType() != EntityType.PLAYER) {
 			return;
 		}
-		
-		ItemStack weapon = ((Player) attackingEntity).getInventory().getItemInMainHand();
-		
+
+		final ItemStack weapon = ((Player) attackingEntity).getInventory().getItemInMainHand();
+
 		if (weapon.isSimilar(SWORD)) {
 			event.setDamage(2);
 		}
