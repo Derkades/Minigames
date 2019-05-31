b78bcb2d9a58250ce068560869d5ed9731ede856
Robin <robinslot@hotmail.nl>
2019-06-01 00:03:36 +0200

    Re-enable all tnt run maps

--- src/xyz/derkades/minigames/games/tntrun/TNTMap.java
+++ src/xyz/derkades/minigames/games/tntrun/TNTMap.java
@@ -6,18 +6,18 @@ import org.bukkit.Material;
 import xyz.derkades.minigames.games.maps.GameMap;
 
 public abstract class TNTMap implements GameMap {
-	
+
 	public static final TNTMap[] MAPS = {
-			//new Aqua(),
-			//new Future(),
-			//new Jungle(),
+			new Aqua(),
+			new Future(),
+			new Jungle(),
 			new WaterLava(),
 	};
-	
+
 	public abstract void restore();
-	
+
 	public abstract Material floorMaterial();
-	
+
 	public abstract Location spawnLocation();
 
 }
