53c6dfbd37d7eccb3f81800f98eaac243d7aca7a
Robin <robinslot@hotmail.nl>
2019-06-01 00:27:42 +0200

    Adjust map chances

--- src/xyz/derkades/minigames/games/maps/MapPicking.java
+++ src/xyz/derkades/minigames/games/maps/MapPicking.java
@@ -10,43 +10,43 @@ import xyz.derkades.minigames.utils.Utils;
 public class MapPicking {
 
 	public static GameMap pickRandomMap(final GameMap[] maps) {
-		Map<GameMap, Double> weightedList = new HashMap<>();
-		
+		final Map<GameMap, Double> weightedList = new HashMap<>();
+
 		// Populate hashmap
-		for (GameMap map : maps) {
+		for (final GameMap map : maps) {
 			final MapSize size = map.getSize();
 			final int online = Bukkit.getOnlinePlayers().size();
-			
+
 			final double weight;
-			
+
 			if (size == MapSize.SMALL) {
 				if (online < 5) {
-					weight = 5;
+					weight = 2;
 				} else {
-					weight = 0.5;
+					weight = 1;
 				}
 			} else if (size == MapSize.NORMAL) {
 				if (online < 4 || online > 6) {
-					weight = 0.5;
+					weight = 1;
 				} else {
-					weight = 5;
+					weight = 2;
 				}
 			} else if (size == MapSize.LARGE) {
 				if (online > 5) {
-					weight = 5;
+					weight = 2;
 				} else {
-					weight = 0.5;
+					weight = 1;
 				}
 			} else {
 				weight = 1;
 			}
-			
+
 			weightedList.put(map, weight);
 		}
-		
-		
+
+
 		return Utils.getWeightedRandom(weightedList);
 	}
-	
+
 
 }
