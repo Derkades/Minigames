923a277bd61cab6690a9ba42bc34afb475b90c1c
Robin <robinslot@hotmail.nl>
2019-06-16 23:42:21 +0200

    redstone cave map

--- src/xyz/derkades/minigames/games/parkour/ParkourMap.java
+++ src/xyz/derkades/minigames/games/parkour/ParkourMap.java
@@ -7,25 +7,26 @@ import org.bukkit.entity.Player;
 import xyz.derkades.minigames.games.maps.GameMap;
 
 public abstract class ParkourMap implements GameMap {
-	
+
 	public static final ParkourMap[] MAPS = {
 			new JungleRun(),
 			//new Plains(),
+			new RedstoneCave(),
 			new Snow(),
 	};
 
 	public abstract Location getStartLocation();
-	
+
 	public abstract Location getSpectatorLocation();
-	
+
 	public abstract boolean hasFinished(Player player, Material blockType);
-	
+
 	public abstract boolean hasDied(Player player, Material blockType);
-	
+
 	public abstract boolean spectatorFreeFlight();
-	
+
 	public abstract int getDuration();
-	
-	public void onPlayerFinish(Player player) {}
-	
+
+	public void onPlayerFinish(final Player player) {}
+
 }
