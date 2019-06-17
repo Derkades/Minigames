f9899f911f7b3bb58a89c8eda6a0421921f14d5b
Robin <robinslot@hotmail.nl>
2019-06-17 15:12:00 +0200

    Allow single player games

--- src/xyz/derkades/minigames/AutoRotate.java
+++ src/xyz/derkades/minigames/AutoRotate.java
@@ -4,13 +4,12 @@ import static org.bukkit.ChatColor.RED;
 
 import org.bukkit.Bukkit;
 
-import net.md_5.bungee.api.ChatColor;
 import xyz.derkades.minigames.games.Game;
 import xyz.derkades.minigames.utils.Scheduler;
 
 public class AutoRotate {
 
-	private static final int MINIMUM_PLAYERS = 2;
+//	private static final int MINIMUM_PLAYERS = 2;
 
 	public static void startNewRandomGame(){
 		if (!Minigames.getInstance().getConfig().getBoolean("autorotate")){
@@ -26,12 +25,18 @@ public class AutoRotate {
 			return;
 		}
 
-		if (Bukkit.getOnlinePlayers().size() < MINIMUM_PLAYERS && !Minigames.BYPASS_PLAYER_MINIMUM_CHECKS){
-			Bukkit.broadcastMessage(ChatColor.RED + "Minigames will only start with 2 or more players online.");
-			Bukkit.broadcastMessage(ChatColor.GOLD + "If you want to have some fun, invite a friend or two!");
-			Scheduler.delay(10*20, AutoRotate::startNewRandomGame);
+		if (Bukkit.getOnlinePlayers().size() < 1) {
+			Bukkit.broadcastMessage("no players online. waiting 5 seconds");
+			Scheduler.delay(5*20, AutoRotate::startNewRandomGame);
 			return;
 		}
+
+//		if (Bukkit.getOnlinePlayers().size() < MINIMUM_PLAYERS && !Minigames.BYPASS_PLAYER_MINIMUM_CHECKS){
+//			Bukkit.broadcastMessage(ChatColor.RED + "Minigames will only start with 2 or more players online.");
+//			Bukkit.broadcastMessage(ChatColor.GOLD + "If you want to have some fun, invite a friend or two!");
+//			Scheduler.delay(10*20, AutoRotate::startNewRandomGame);
+//			return;
+//		}
 
 		// If a next game is set by a command, do that game next. Otherwise, pick a random game.
 		final Game game = Minigames.NEXT_GAME == null ? Game.getRandomGame() : Minigames.NEXT_GAME;
