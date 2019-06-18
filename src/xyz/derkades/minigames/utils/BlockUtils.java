548f39170456d3621be7aff732daab15c047820c
Robin <robinslot@hotmail.nl>
2019-06-19 01:45:40 +0200

    Add world specific fill methods

--- src/xyz/derkades/minigames/utils/BlockUtils.java
+++ src/xyz/derkades/minigames/utils/BlockUtils.java
@@ -4,6 +4,7 @@ import java.util.function.Consumer;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.block.Block;
 
 import xyz.derkades.minigames.Var;
@@ -17,6 +18,7 @@ public class BlockUtils {
 				a.getWorld().getName().equals(b.getWorld().getName()));
 	}
 
+	@Deprecated
 	public static void fillArea(final int x1, final int y1, final int z1, final int x2, final int y2, final int z2, final Material material) {
 		final int minX = Math.min(x1, x2);
 		final int minY = Math.min(y1, y2);
@@ -34,6 +36,7 @@ public class BlockUtils {
 		}
 	}
 
+	@Deprecated
 	public static void fillArea(final int x1, final int y1, final int z1, final int x2, final int y2, final int z2, final Material material, final Consumer<Block> runForEveryBlock) {
 		final int minX = Math.min(x1, x2);
 		final int minY = Math.min(y1, y2);
@@ -46,6 +49,42 @@ public class BlockUtils {
 			for (int y = minY; y <= maxY; y++) {
 				for (int z = minZ; z <= maxZ; z++) {
 					final Block block = new Location(Var.WORLD, x, y, z).getBlock();
+					block.setType(material);
+					runForEveryBlock.accept(block);
+				}
+			}
+		}
+	}
+
+	public static void fillArea(final World world, final int x1, final int y1, final int z1, final int x2, final int y2, final int z2, final Material material) {
+		final int minX = Math.min(x1, x2);
+		final int minY = Math.min(y1, y2);
+		final int minZ = Math.min(z1, z2);
+		final int maxX = Math.max(x1, x2);
+		final int maxY = Math.max(y1, y2);
+		final int maxZ = Math.max(z1, z2);
+
+		for (int x = minX; x <= maxX; x++) {
+			for (int y = minY; y <= maxY; y++) {
+				for (int z = minZ; z <= maxZ; z++) {
+					new Location(world, x, y, z).getBlock().setType(material);
+				}
+			}
+		}
+	}
+
+	public static void fillArea(final World world, final int x1, final int y1, final int z1, final int x2, final int y2, final int z2, final Material material, final Consumer<Block> runForEveryBlock) {
+		final int minX = Math.min(x1, x2);
+		final int minY = Math.min(y1, y2);
+		final int minZ = Math.min(z1, z2);
+		final int maxX = Math.max(x1, x2);
+		final int maxY = Math.max(y1, y2);
+		final int maxZ = Math.max(z1, z2);
+
+		for (int x = minX; x <= maxX; x++) {
+			for (int y = minY; y <= maxY; y++) {
+				for (int z = minZ; z <= maxZ; z++) {
+					final Block block = new Location(world, x, y, z).getBlock();
 					block.setType(material);
 					runForEveryBlock.accept(block);
 				}
