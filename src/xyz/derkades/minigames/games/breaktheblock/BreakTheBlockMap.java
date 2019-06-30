dcd33cd514b4a68af625cbfd12303b7d089e6ac0
Robin <42698687+patchtv@users.noreply.github.com>
2019-06-30 23:05:16 +0200

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- src/xyz/derkades/minigames/games/breaktheblock/BreakTheBlockMap.java
+++ src/xyz/derkades/minigames/games/breaktheblock/BreakTheBlockMap.java
@@ -8,6 +8,7 @@ public abstract class BreakTheBlockMap extends GameMap {
 
 	public static final BreakTheBlockMap[] MAPS = {
 			new Prototype(),
+			new Jungle(),
 	};
 
 	public abstract void onPreStart();
---
+++ src/xyz/derkades/minigames/games/breaktheblock/Jungle.java
@@ -0,0 +1,53 @@
+package xyz.derkades.minigames.games.breaktheblock;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+
+import xyz.derkades.minigames.Minigames;
+import xyz.derkades.minigames.games.maps.MapSize;
+import xyz.derkades.minigames.utils.MPlayer;
+import xyz.derkades.minigames.worlds.GameWorld;
+
+public class Jungle extends BreakTheBlockMap {
+
+	@Override
+	public void onPreStart() {
+	}
+
+	@Override
+	public void onStart() {
+		new Location(this.getWorld(), 38, 65, -21).getBlock().setType(Material.GOLD_BLOCK);
+
+	}
+
+	@Override
+	public void timer() {
+		for (MPlayer player : Minigames.getOnlinePlayers()) {
+			if (player.getBlockIn().getType() == Material.LAVA ) {
+				player.teleport(getStartLocation());
+				player.removeFire();
+			}
+		}
+	}
+
+	@Override
+	public Location getStartLocation() {
+		return new Location(this.getWorld(), 0.5, 65, 0.5, -123f, 14f);
+	}
+
+	@Override
+	public String getName() {
+		return "Jungle";
+	}
+
+	@Override
+	public MapSize getSize() {
+		return null;
+	}
+
+	@Override
+	public GameWorld getGameWorld() {
+		return GameWorld.BTB_JUNGLE;
+	}
+
+}
