955e8438b0861f222441269debf63f052c2977d6
Robin <robinslot@hotmail.nl>
2019-10-06 21:45:12 +0200

    random 1-10

--- src/xyz/derkades/minigames/Command.java
+++ src/xyz/derkades/minigames/Command.java
@@ -5,6 +5,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import net.md_5.bungee.api.ChatColor;
+import xyz.derkades.derkutils.Random;
 import xyz.derkades.minigames.Minigames.ShutdownReason;
 import xyz.derkades.minigames.board.DiceAnimationMenu;
 import xyz.derkades.minigames.menu.GamesListMenu;
@@ -70,7 +71,7 @@ public class Command implements CommandExecutor {
 				Minigames.getInstance().saveConfig();
 				sender.sendMessage("Set debug mode to " + Logger.debugMode);
 			} else if (args[0].equals("test") && sender.hasPermission("minigames.test")) {
-				new DiceAnimationMenu((Player) sender, 1, 10, 5).open();
+				new DiceAnimationMenu((Player) sender, 1, 10, Random.getRandomInteger(1, 10)).open();
 //		        final Location fbLocation = loc.add(
 //		        		loc
 //		                .getDirection()
