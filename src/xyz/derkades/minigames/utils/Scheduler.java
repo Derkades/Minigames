77400a766cfd601dbf173a9f38c988584ea798d9
Robin <robinslot@hotmail.nl>
2019-10-07 11:47:15 +0200

    Return BukkitTask

--- src/xyz/derkades/minigames/utils/Scheduler.java
+++ src/xyz/derkades/minigames/utils/Scheduler.java
@@ -1,29 +1,30 @@
 package xyz.derkades.minigames.utils;
 
 import org.bukkit.Bukkit;
+import org.bukkit.scheduler.BukkitTask;
 
 import xyz.derkades.minigames.Minigames;
 
 public class Scheduler {
-	
-	public static void delay(long delay, Runnable runnable) {
-		Bukkit.getScheduler().runTaskLater(Minigames.getInstance(), runnable, delay);
+
+	public static BukkitTask delay(final long delay, final Runnable runnable) {
+		return Bukkit.getScheduler().runTaskLater(Minigames.getInstance(), runnable, delay);
 	}
-	
-	public static void repeat(long interval, Runnable runnable) {
-		repeat(0, interval, runnable);
+
+	public static BukkitTask repeat(final long interval, final Runnable runnable) {
+		return repeat(0, interval, runnable);
 	}
-	
-	public static void repeat(long delay, long interval, Runnable runnable) {
-		Bukkit.getScheduler().runTaskTimer(Minigames.getInstance(), runnable, delay, interval);
+
+	public static BukkitTask repeat(final long delay, final long interval, final Runnable runnable) {
+		return Bukkit.getScheduler().runTaskTimer(Minigames.getInstance(), runnable, delay, interval);
 	}
-	
-	public static void run(Runnable runnable) {
-		Bukkit.getScheduler().runTask(Minigames.getInstance(), runnable);
+
+	public static BukkitTask run(final Runnable runnable) {
+		return Bukkit.getScheduler().runTask(Minigames.getInstance(), runnable);
 	}
-	
-	public static void async(Runnable runnable) {
-		Bukkit.getScheduler().runTaskAsynchronously(Minigames.getInstance(), runnable);
+
+	public static BukkitTask async(final Runnable runnable) {
+		return Bukkit.getScheduler().runTaskAsynchronously(Minigames.getInstance(), runnable);
 	}
-	
+
 }
