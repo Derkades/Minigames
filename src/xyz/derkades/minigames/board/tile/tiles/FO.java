69d8254410ee50605b5f4410e0423fd829957743
Daniel <52836397+DanielS-Melon@users.noreply.github.com>
2019-10-14 11:22:00 +0200

    FO reeeeeeee

--- src/xyz/derkades/minigames/board/tile/tiles/FO.java
+++ src/xyz/derkades/minigames/board/tile/tiles/FO.java
@@ -1,5 +1,6 @@
 package xyz.derkades.minigames.board.tile.tiles;
 
+import xyz.derkades.minigames.board.BoardPlayer;
 import xyz.derkades.minigames.board.tile.SwapPositionsTile;
 import xyz.derkades.minigames.board.tile.Tile;
 import xyz.derkades.minigames.utils.XYZ;
@@ -12,8 +13,14 @@ public class FO extends SwapPositionsTile {
 	}
 
 	@Override
-	public XYZ getXYZ() {
+	public XYZ getXYZ() { 
 		return new XYZ(151, 140, 77);
+	}
+
+	@Override
+	public void landOnTile(BoardPlayer player) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
