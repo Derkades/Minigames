000123b809744d83e7cc089e0964053be47df5fd
Derkades <robinslot@hotmail.nl>
2021-01-13 00:36:07 +0100

    More accurate name

--- src/xyz/derkades/minigames/games/maps/GameMap.java
+++ src/xyz/derkades/minigames/games/maps/GameMap.java
@@ -63,7 +63,7 @@ public abstract class GameMap implements RandomlyPickable {
 	
 	private static final Map<String, GameMap> BY_IDENTIFIER = new HashMap<>();
 	
-	public static final void checkMaps() {
+	public static final void init() {
 		for (final Game<? extends GameMap> game : Game.GAMES) {
 			for (final GameMap map : game.getGameMaps()) {
 				if (BY_IDENTIFIER.containsKey(map.getIdentifier())) {
