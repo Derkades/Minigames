dccc98b5eba2d5ff268bd7048c6dcd213b63bc0f
Robin <robinslot@hotmail.nl>
2021-09-16 23:24:42 +0200

    show name color in join/leave message

--- src/derkades/minigames/GlobalListeners.java
+++ src/derkades/minigames/GlobalListeners.java
@@ -28,12 +28,12 @@ import net.md_5.bungee.api.ChatColor;
 
 public class GlobalListeners implements Listener {
 
-	private Component getJoinLeaveMessage(final String name, final ChatColor color, final char c) {
+	private Component getJoinLeaveMessage(final Component name, final ChatColor color, final char c) {
 		final TextColor textColor = TextColor.color(color.getColor().getRGB());
 		return Component.text("[")
 				.append(Component.text(c).color(textColor))
 				.append(Component.text("] "))
-				.append(Component.text(name).color(textColor));
+				.append(name);
 	}
 
 	@EventHandler
@@ -41,7 +41,7 @@ public class GlobalListeners implements Listener {
 		final MPlayer player = new MPlayer(event.getPlayer());
 
 //		event.setJoinMessage(String.format("[%s+%s] %s| %s%s", ChatColor.GREEN, ChatColor.RESET, ChatColor.DARK_GRAY, ChatColor.GREEN, player.getName()));
-		event.joinMessage(getJoinLeaveMessage(player.getName(), ChatColor.GREEN, '+'));
+		event.joinMessage(getJoinLeaveMessage(player.bukkit().displayName(), ChatColor.GREEN, '+'));
 
 		// Anti collision
 		Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "team join global " + player.getName());
@@ -85,7 +85,7 @@ public class GlobalListeners implements Listener {
 	public void onQuit(final PlayerQuitEvent event){
 		final MPlayer player = new MPlayer(event);
 //		event.setQuitMessage(String.format("[%s-%s] %s| %s%s", ChatColor.RED, ChatColor.RESET, ChatColor.DARK_GRAY, ChatColor.RED, player.getName()));
-		event.quitMessage(getJoinLeaveMessage(player.getName(), ChatColor.RED, '-'));
+		event.quitMessage(getJoinLeaveMessage(player.bukkit().displayName(), ChatColor.RED, '-'));
 
 		if (GameState.getCurrentState().isInGame()) {
 			GameState.getCurrentGame().onPlayerQuit(player); // TODO move to game class?
