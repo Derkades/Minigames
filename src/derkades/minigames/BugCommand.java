f5df8083cbb98cc08cff38f3eb38e7cfdfaf9639
Robin <robinslot@hotmail.nl>
2021-10-02 00:34:11 +0200

    fix #298

--- src/derkades/minigames/BugCommand.java
+++ src/derkades/minigames/BugCommand.java
@@ -1,18 +1,20 @@
 package derkades.minigames;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-import org.jetbrains.annotations.NotNull;
-import org.kohsuke.github.GHRepository;
-import org.kohsuke.github.GitHub;
-
 import derkades.minigames.utils.Scheduler;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.event.ClickEvent;
 import net.kyori.adventure.text.event.HoverEvent;
 import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import org.kohsuke.github.GHIssue;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+
+import java.net.URL;
 
 public class BugCommand implements CommandExecutor {
 
@@ -20,15 +22,6 @@ public class BugCommand implements CommandExecutor {
 	private static final Component SUBMITTING = Component.text("Submitting feedback...", NamedTextColor.GRAY);
 	@NotNull
 	private static final Component UNAVAILABLE = Component.text("Sorry, this command is temporarily unavailable.", NamedTextColor.GRAY);
-	@NotNull
-	private static final Component THANKS = Component.text()
-			.append(Component.text("Thanks for letting us know. You can view all open issues ")
-					.color(NamedTextColor.GRAY))
-			.append(Component.text("here").color(NamedTextColor.YELLOW)
-					.hoverEvent(HoverEvent.showText(
-							Component.text("Click to visit https://github.com/Derkades/Minigames/issues").color(NamedTextColor.GRAY)))
-					.clickEvent(ClickEvent.openUrl("https://github.com/Derkades/Minigames/issues")))
-			.append(Component.text(".").color(NamedTextColor.GRAY)).build();
 
 	@Override
 	public boolean onCommand(final @NotNull CommandSender sender, final @NotNull Command arg1, final @NotNull String label, final String[] args) {
@@ -60,14 +53,23 @@ public class BugCommand implements CommandExecutor {
 				final GitHub github = GitHub.connectUsingOAuth(Minigames.getInstance().getConfig().getString("github-token"));
 				final GHRepository repo = github.getRepository("Derkades/Minigames");
 				final String description = String.join(" ", args);
-				/*final GHIssue issue = */repo.createIssue(description).body(description + "\n\n" + "This issue was submitted in-game by '" + player.getName() + "'.").create();
+				final GHIssue issue = repo.createIssue(description).body(description + "\n\n" + "This issue was submitted in-game by '" + player.getName() + "'.").create();
 //				if (issueLabel == null) {
 //					issue.addLabels(repo.getLabel("bot"));
 //				} else {
 //					issue.addLabels(repo.getLabel("bot"), repo.getLabel(issueLabel));
 //				}
 
-				player.sendMessage(THANKS);
+				URL url = issue.getUrl();
+
+				player.sendMessage(Component.text()
+						.append(Component.text("Thanks for letting us know. You can view your issue ")
+								.color(NamedTextColor.GRAY))
+						.append(Component.text("here").color(NamedTextColor.YELLOW)
+								.hoverEvent(HoverEvent.showText(
+										Component.text("Click to visit " + url.toExternalForm()).color(NamedTextColor.GRAY)))
+								.clickEvent(ClickEvent.openUrl(url))
+						.append(Component.text(".").color(NamedTextColor.GRAY))));
 			} catch (final Exception e) {
 				Logger.warning("%s encountered an issue while trying to create an issue", sender.getName());
 				player.sendMessage("An error occured while trying to create an issue.");
