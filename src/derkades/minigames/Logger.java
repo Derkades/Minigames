a583dd111f939246223b8c9a5fe0bb0132521749
Robin <robinslot@hotmail.nl>
2021-10-15 17:23:21 +0200

    fix stackoverflow and other logic issues

--- src/derkades/minigames/Logger.java
+++ src/derkades/minigames/Logger.java
@@ -1,45 +1,57 @@
 package derkades.minigames;
 
+import derkades.minigames.utils.Scheduler;
+import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-
-import net.md_5.bungee.api.ChatColor;
 
 public class Logger {
 
 	static boolean debugMode = Minigames.getInstance().getConfig().getBoolean("debug_mode");
 
-	public static void warning(final String message, final Object... arg1) {
+	public static void warning(final String message, final Object... args) {
 		if (!Bukkit.isPrimaryThread()) {
-			Logger.warning("Logger was called asynchronously!");
+			Scheduler.run(() -> {
+				warning("Logger was called asynchronously! Message: %s", message);
+				warning(message, args);
+			});
+			return;
 		}
 
-		final String formatted = String.format(message, arg1);
+		final String formatted = String.format(message, args);
 		Minigames.getInstance().getLogger().warning(formatted);
 		if (debugMode) {
 			broadcast(ChatColor.RED + "Warning: " + ChatColor.GRAY + formatted);
 		}
 	}
 
-	public static void info(final String message, final Object... arg1) {
+	public static void info(final String message, final Object... args) {
 		if (!Bukkit.isPrimaryThread()) {
-			Logger.warning("Logger was called asynchronously!");
+			Scheduler.run(() -> {
+				warning("Logger was called asynchronously! Message: %s", message);
+				info(message, args);
+			});
+			return;
 		}
 
-		final String formatted = String.format(message, arg1);
+		final String formatted = String.format(message, args);
 		Minigames.getInstance().getLogger().info(formatted);
 		if (debugMode) {
 			broadcast(ChatColor.BLUE + "Info: " + ChatColor.GRAY + formatted);
 		}
 	}
 
-	public static void debug(final String message, final Object... arg1) {
+	public static void debug(final String message, final Object... args) {
 		if (!Bukkit.isPrimaryThread()) {
-			Logger.warning("Logger was called asynchronously!");
+			Scheduler.run(() -> {
+				warning("Logger was called asynchronously! Message: %s", message);
+				info(message, args);
+			});
+			return;
 		}
 
 		if (debugMode) {
-			final String formatted = String.format(message, arg1);
+			final String formatted = String.format(message, args);
 			Minigames.getInstance().getLogger().info(formatted);
 			broadcast(ChatColor.GRAY + "Debug: " + ChatColor.DARK_GRAY + formatted);
 		}
